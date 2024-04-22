50a6b8304c83f0025d9d8a087a59cb918fffb224
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-04-22 21:13:31 +0300

    YandexView

--- My-portfolio/main.395992eae8c477afb1b8.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";var e=document.querySelector(".header__burger"),t=document.querySelector(".header");e.addEventListener("click",(function(){t.classList.toggle("open")})),document.querySelectorAll(".header__link").forEach((function(e){e.addEventListener("click",(function(){t.classList.remove("open")}))}))}();
